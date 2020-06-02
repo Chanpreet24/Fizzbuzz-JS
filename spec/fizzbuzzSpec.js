74bf26152cb5c453be3f8359882cbc6f5114f5cb
Student <student@Makerss-Air-2.broadband>
2020-06-02 14:50:42 +0100

    Create Fizzbuzz

--- spec/fizzbuzzSpec.js
+++ spec/fizzbuzzSpec.js
@@ -10,5 +10,25 @@ describe('Fizzbuzz', function() {
         it('fizzes for 3', function() {
           expect(fizzbuzz.play(3)).toEqual('Fizz');
         });
+        
+        it('fizzes for 6', function() {
+            expect(fizzbuzz.play(6)).toEqual('Fizz');
+        });
+    });
+
+    describe('multiples of 5', function() {
+        it('buzzes for 5', function(){
+            expect(fizzbuzz.play(5)).toEqual('Buzz')
+        });
+
+        it('buzzes for 10', function(){
+            expect(fizzbuzz.play(10)).toEqual('Buzz')
+        });
+    });
+
+    describe('multiples of 15', function() {
+        it('We have a Fizzbuzz for 15', function() {
+            expect(fizzbuzz.play(15)).toEqual('FizzBuzz')
+        });
     });
 });
--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,11 +1,12 @@
 function Fizzbuzz(){
     this.play = function(number){
-        if (number/3){
+        if ( ((number%3) == 0) && ((number%5) == 0)){
+            return "FizzBuzz"
+        }
+        else if ((number%3) == 0){
             return "Fizz"
+        }else if((number%5) == 0){
+            return "Buzz"
         }
     }
 }
-
-window.addEventListener("load", function(){
-    Fizzbuzz();
-});
